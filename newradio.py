08a2662a42075879826e2fd3c77da84ec816e14a
Davi <davisbrilhante@gmail.com>
2019-09-25 15:49:21 -0300

    adding the code in cpp to calc the snr resulting from ia

--- newradio.py
+++ newradio.py
@@ -14,13 +14,13 @@ SUBFRAME_DURATION=1
 BURST_DURATION=5
 BURST_PERIOD=20
 RACH_PERIOD=40
-SIM_DURATION=60000
-LTE_RTT=4
+SIM_DURATION=10000
+LTE_RTT=1
 ENV_RADIUS=50
 SPACE=[[0 for j in range(2*ENV_RADIUS+1)] for i in range(2*ENV_RADIUS+1)]
 
 
-def numerology(subcarrierSpacing):
+def numerology(subcarrierSpacing, ):
     """
     Defines the time numerology based on the frame structure of 5G NR
     Needs the the subcarrier spacing in kHz, e.g. 15 means 15 kHz
@@ -30,13 +30,17 @@ def numerology(subcarrierSpacing):
         'ofdmSymbolDuration':0,
         'slotDuration':SUBFRAME_DURATION,
         'minRB':20,
-        'maxRB':275,
-        'ss_blocks':[0 for i in range(14)]
+        'maxRB':275
+        #'ssblocks':4,
+        #'ssblockMapping':[0 for i in range(14)]
     }
 
     if subcarrierSpacing == 15:
         numerol['ofdmSymbolDuration'] = SUBFRAME_DURATION/14
-        numerol['ss_blocks'] = []
+#        if numerol['ssblocks']==4:
+#            numerol['ssblockMapping'] = []
+#        elif numerol['ssblocks']==8:
+#            numerol['ssblockMapping'] = []
         return numerol
 
     elif subcarrierSpacing == 30:
@@ -90,22 +94,20 @@ class Network(object):
         burstPeriod = 20 milliseconds
         rachPeriod = 40 milliseconds
         '''
-        counter = 1
         while True:
-            if (counter % (1 + rachPeriod/burstPeriod)) != 0:
+            if (self.ssbIndex % (rachPeriod/burstPeriod)) != 0:
                 print('A new burst set is starting at %d' % self.env.now)
                 yield self.env.timeout(burstDuration)
                 print('The burst set has finished at %d' % self.env.now)
                 yield self.env.timeout(burstPeriod - burstDuration)
             else:
                 yield self.env.timeout(burstPeriod)
-            counter += 1
 
     def updateFrame(self):
         while True:
             yield self.env.timeout(FRAME_DURATION)
             self.frameIndex+=1
-            if self.ssbIndex % 2 == 1:
+            if self.frameIndex % 2 == 1:
                 self.ssbIndex+=1
 
     def rachOpportunity(self, rachDuration, rachPeriod):
@@ -117,11 +119,14 @@ class Network(object):
         rachOpportunity = 40 milliseconds
         '''
         while True:
-            yield self.env.timeout(rachPeriod)
-            print('A new rach opportunity is starting at %d' % self.env.now)
-            yield self.env.timeout(rachDuration)
-            print('The rach opportunity  has finished at %d' % self.env.now)
-            yield self.env.timeout(rachPeriod - rachDuration)
+            ### Grants a SS Burst at the first frame but avoids a RACH at the first frame
+            if self.frameIndex==1:
+                yield self.env.timeout(rachPeriod)
+            else:
+                print('A new rach opportunity is starting at %d' % self.env.now)
+                yield self.env.timeout(rachDuration)
+                print('The rach opportunity  has finished at %d' % self.env.now)
+                yield self.env.timeout(rachPeriod - rachDuration)
 
     def initializeServices(self):
         self.env.process(self.updateFrame())
@@ -145,18 +150,52 @@ class Network(object):
         timeSpent = 0
         neededSSB = 0
         for user in self.inRangeusers:
-            dist = str(self.calcUserDist(user))
+            Pt = ' 30'
+            dist = str(self.calcUserDist(user)) #{5 10 15 20 30 40 50 60 70 80 90 100 140 180 200}; # VOU PASSAR ESSE VALOR
+            npontos = ' 50'
+            seed = sys.argv[5]
+            NF = ' 5'
+            TN = ' -174'
+            BW = ' 400000000' #1000000000
+            div = ' 4'
+            move = ' 75000'
+            minSNR = ' -5'
+            Tper = ' 1'
+            Tcanal = ' 1'
+            protoID = ' 1'
+            protoParam = ' 500'
+            limite = protoParam # O QUE SER ISSO?
+            tipoErro = ' 1'
+            mediaErroGPS = sys.argv[4]
+            desvErroGPS = ' 10'
+            alg = algorithm #sys.argv[1]
+            log= ' 0'
+            velocityUSR = ' 0'
+            velocityOBJ = ' 5'
+            decaimentoTaxaRx = protoParam # O QUE SER ISSO 
+            quedaTaxaRx = protoParam #O QUE SER ISSO
+            fastIA = ' 0'
+            limFastIA = ' 0'
+            condCanal = condition #sys.argv[2]
             angle = str(self.calcUserAngle(user))
             bs_array = str(self.antennaArray[0])
             ue_array = str(user.antennaArray[0])
+            arqname = ' initial-access-'+alg+'-'+condCanal+'-'+mediaErroGPS+'-'+seed
+
+
+            command = ('./initial-access'+Pt+' '+dist+npontos+' '+seed+arqname+NF+TN+BW+div+move+minSNR+Tper+Tcanal+limite+tipoErro
+                      +' '+mediaErroGPS+desvErroGPS+' '+alg+log+velocityUSR+velocityOBJ+protoID+decaimentoTaxaRx+quedaTaxaRx+fastIA+limFastIA
+                      +' '+condCanal+' '+condCanal+' '+angle+' '+bs_array+' '+ue_array)
+            print(command)
             try:
-                result = call('simu-alex/initial-access'+' my-args', shell=True)
+                result = call(command, shell=True)
                 if result < 0:
                     print("initial-access was terminated by signal", -result, file=sys.stderr)
                 else:
-                    print("initial-access returned", result, file=sys.stderr)
+                    print("initial-access returned", result)#, file=sys.stderr)
             except:
                 print("Execution failed:", e, file=sys.stderr)
+
             if algorithm == '0': #exhaustive
                 1
 
@@ -169,22 +208,25 @@ class Network(object):
 
             elif algorithm == '3': #refined+search
                 neededSSB = 5
+        self.inRangeUsers=[]
             
 
     def associationRequest(self,user):
+        print("chamou a callback")
         algorithm = sys.argv[1]
         condition = sys.argv[2]
         #The search is exhaustive, so the search is a little different
         if algorithm == '0':
             #UE join the network before the nearest SSB
-            if self.env.now < (self.ssbIndex+1)*SSB_DURATION:
+            if self.env.now < (self.ssbIndex+1)*BURST_DURATION:
                 1
-            #UE join the network during a SSB
-            elif self.env.now < (self.ssbIndex+1)*SSB_DURATION + 5:
+            #UE join the network during a BURST
+            elif self.env.now < (self.ssbIndex+1)*BURST_DURATION + 5:
                 1
         #The search is not exhaustive
         else:
-            if self.env.now + LTE_RTT < (self.ssbIndex+1)*SSB_DURATION:
+            print("Condition: ",(self.ssbIndex+1)*BURST_PERIOD)
+            if self.env.now + LTE_RTT < (self.ssbIndex+1)*BURST_DURATION:
                 '''
                 In this occasion, the message containig the lacation had the time
                 to travel through the LTE control channel before the next SS Burst
@@ -240,7 +282,7 @@ class Scenario(object):
                 yield self.env.timeout(skip)
 
     def initializeUsers(self, arrivalRate, skipRate, nUsers):
-        env.process(self.userArrival(arrivalRate,ENV_RADIUS))
+        env.process(self.userArrival(arrivalRate,ENV_RADIUS, self.network.associationRequest))
         env.process(self.userSkip(nUsers,skipRate))
 
         
@@ -260,9 +302,9 @@ if __name__ == "__main__":
     """
     Scheduling nerwork processes
     """
-    scenario = Scenario(env)
-    scenario.initializeUsers(arrivalRate, skipRate, nUsers)
     fiveG = Network(env,[8,8])
     fiveG.initializeServices()
+    scenario = Scenario(env,fiveG)
+    scenario.initializeUsers(arrivalRate, skipRate, nUsers)
     #time in milliseconds
     env.run(until=SIM_DURATION)
