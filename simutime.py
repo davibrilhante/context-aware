3bae76034f74dfaaab30bf101f107dd472269c4f
dbrilhante <davisbrilhante@gmail.com>
2019-10-01 22:46:55 -0300

    time conversions

--- simutime.py
+++ simutime.py
@@ -1,66 +1,67 @@
-class Time:
-    def __init__(self):
-        self.type = None
-        self.amount = 0
+class seconds:
+    def __init__(self, amount):
+        self.amount = amount
+    '''
+    def year(self):
+        return self.amount/(365*24*60*60)
+    def month(self): 
+        return self.amount/(30*24*60*60)
+    def week(self): 
+        return self.amount/(7*24*60*60)
+    def day(self): 
+        return self.amount/(24*60*60)
+    '''
+    def hour(self): 
+        return self.amount/60*60
+    def minute(self): 
+        return self.amount/60
+    def milli(self): 
+        return self.amount*1e3
+    def micro(self): 
+        return self.amount*1e6
+    def nano(self): 
+        return self.amount*1e9
 
-    def seconds(self, amount):
-        if self.type == 'year':
-            self.amount = amount*365*24*60*60
-        elif self.type == 'month':
-            self.amount = amount*30*24*60*60
-        elif self.type == 'week':
-            self.amount = amount*7*24*60*60
-        elif self.type == 'day':
-            self.amount = amount*24*60*60
-        elif self.type =='hour':
-            self.amount = amount*60*60
-        elif self.type =='minute':
-            self.amount = amount*60
-        elif self.type == None or self.type == 'seconds':
-            self.amount = amount
-        elif self.type =='milli':
-            self.amount = amount/1e3
-        elif self.type =='micro':
-            self.amount = amount/1e6
-        elif self.type =='nano':
-            self.amount = amount/1e9
-        self.type = 'seconds'
+class milliseconds:
+    def __init__(self, amount):
+        self.amount = amount
+    def minute(self):
+        return self.amount/(60*1e3)
+    def seconds(self): 
+        return self.amount/1e3
+    def micro(self): 
+        return self.amount*1e3
+    def nano(self): 
+        return self.amount*1e6
 
-    def milliseconds(self, amount):
-        if self.type =='minute':
-            self.amount = amount*60*1e3
-        elif self.type == 'seconds':
-            self.amount = amount*1e3
-        elif self.type =='milli' or self.type ==None:
-            self.amount = amount
-        elif self.type =='micro':
-            self.amount = amount/1e3
-        elif self.type =='nano':
-            self.amount = amount/1e6
-        self.type = 'milli'
 
-    def microseconds(self, amount):
-        if self.type =='minute':
-            self.amount = amount*60*1e6
-        elif self.type == 'seconds':
-            self.amount = amount*1e6
-        elif self.type =='milli':
-            self.amount = amount*1e3
-        elif self.type =='micro' or self.type ==None:
-            self.amount = amount
-        elif self.type =='nano':
-            self.amount = amount/1e3
-        self.type = 'micro'
+class microseconds:
+    def __init__(self, amount):
+        self.amount = amount
+    def minute(self):
+        return self.amount/(60*1e6)
+    def seconds(self): 
+        return self.amount/1e6
+    def milli(self): 
+        return self.amount/1e3
+    def nano(self): 
+        return self.amount*1e3
 
-    def microseconds(self, amount):
-        if self.type =='minute':
-            self.amount = amount*60*1e9
-        elif self.type == 'seconds':
-            self.amount = amount*1e9
-        elif self.type =='milli':
-            self.amount = amount*1e6
-        elif self.type =='micro':
-            self.amount = amount*1e3
-        elif self.type =='nano' or self.type ==None:
-            self.amount = amount
-        self.type = 'nano'
+
+class nanoseconds:
+    def __init__(self, amount):
+        self.amount = amount
+    def minute(self):
+        return self.amount/(60*1e9)
+    def seconds(self): 
+        return self.amount/1e9
+    def milli(self): 
+        return self.amount/1e6
+    def micro(self): 
+        return self.amount/1e3
+'''
+def divide(a,b):
+    if type(a) == type(b):
+        return a.amount/b.amount
+    else:
+'''
