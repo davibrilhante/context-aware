63e60a4c7469d30305a2ab62917d2cc7de0398a7
dbrilhante <davisbrilhante@gmail.com>
2020-02-11 13:21:26 +0000

    added arguments, mainly the number of algorithm slots

--- numerical.py
+++ numerical.py
@@ -1,19 +1,41 @@
 import numpy as np
 from matplotlib import pyplot as plt
 import sys
+import argparse
 
+
+###########################################
+#               Constants
+##########################################
 k = 1.380649e-23
 t = 300
 N0 = k*t
 
-B_BS = 16
-B_UE = 4
+###########################################
+#               Arguments
+###########################################
+parser = argparse.ArgumentParser()
+parser.add_argument('-p', '--plotType',help='The type of plot you want', default=1, type=int)
+parser.add_argument('-b', '--bsAntennas',help='The number of base station antenna elements', default=16, type=int, required=False)
+parser.add_argument('-u', '--ueAntennas',help='The number of User Equipment antenna elements', default=4, type=int, required=False)
+parser.add_argument('-t', '--txPower',help='The transmitter power in watts', default=1, type=int, required=False)
+parser.add_argument('-l', '--pathLoss',help='The path loss in dB', default=-90, type=float, required=False)
+parser.add_argument('-s', '--subcarrierSpacing',help='Numerology', default=3, type=float, required=False)
+parser.add_argument('-n', '--extraUsers',help='Number of extra users sharing', default=0, type=int, required=False)
+#parser.add_argument('-a','--availableSubcarriers', help='Available subcarriers', default=3300, type=int, required=False)
+parser.add_argument('-a','--algorithmSlots',help='Number of slots taken by the algorithm to complete IA',default=5,type=int,required=False)
 
-tau_ofdm = 8.9e-6
+args = parser.parse_args()
+
+plotType = args.plotType
+B_BS = args.bsAntennas
+B_UE = args.ueAntennas
+p_tx = args.txPower #30 dBm
+pl = args.pathLoss
+tau_ofdm = (1e-3/(2**(args.subcarrierSpacing)))/14
 tau_ssb = 4*tau_ofdm
 
-p_tx = 1 #30 dBm
-pl = -90
+userArray = [int(i*64/(3*args.algorithmSlots)) for i in range(4)]
 
 
 if pl < 0:
@@ -21,7 +43,7 @@ if pl < 0:
 else:
     path_loss_w = pl
 
-if sys.argv[1] == '1':
+if plotType == 1:
     system_capacity = []
     for s in range(241,3300):
         nSweeps = 64/B_BS
@@ -30,8 +52,9 @@ if sys.argv[1] == '1':
     plt.plot(system_capacity, label='exh', linestyle='--')
 
 
-    algorithm_slots = 5
-    for n in [0,4,8,12]:
+    algorithm_slots = args.algorithmSlots
+    #for n in [0,4,8,12]:
+    for n in userArray:
         system_capacity = []
         for s in range(241,3300):
             temp = (64 - (n*algorithm_slots))*(s/tau_ssb)*np.log(1 + ((tau_ssb/s)*(p_tx*path_loss_w/N0)))
@@ -45,8 +68,8 @@ if sys.argv[1] == '1':
 
     
 
-elif sys.argv[1] == '2':
-    algorithm_slots = 5
+elif plotType == 2:
+    algorithm_slots = args.algorithmSlots
     ssb_capacity = []
     for n in range(1,int(np.floor(64/algorithm_slots))+1):
         temp = (64 - (n*algorithm_slots))*(240/tau_ssb)*np.log(1 + ((tau_ssb/240)*(p_tx*path_loss_w/N0)))
@@ -57,7 +80,7 @@ elif sys.argv[1] == '2':
     plt.xlabel('Users Arriving in the Burst Set')
     plt.ylabel('Burst Set Downlink Capacity')
 
-if sys.argv[1] == '3':
+if plotType == 3:
     system_capacity = []
     for s in range(241,3300):
         nSweeps = 64/B_BS
@@ -67,8 +90,9 @@ if sys.argv[1] == '3':
     plt.plot(system_capacity, label='exh', linestyle='--')
 
 
-    algorithm_slots = 5
-    for n in [0,4,8,12]:
+    algorithm_slots = args.algorithmSlots
+    #for n in [0,4,8,12]:
+    for n in userArray:
         system_capacity = []
         for s in range(241,3300):
             temp = (64 - (n*algorithm_slots))*(s/tau_ssb)*np.log(1 + ((tau_ssb/s)*(p_tx*path_loss_w/N0)))
@@ -81,9 +105,10 @@ if sys.argv[1] == '3':
     plt.ylabel('Burst Set Downlink Capacity')
     plt.legend()
 
-if sys.argv[1] == '4':
-    algorithm_slots = 5
-    for n in [0,4,8,12]:
+if plotType == 4:
+    algorithm_slots = args.algorithmSlots
+    #for n in [0,4,8,12]:
+    for n in userArray:
         system_capacity = []
         for s in range(241,3300):
             nSweeps = 64/B_BS
@@ -100,9 +125,10 @@ if sys.argv[1] == '4':
     plt.ylabel('Burst Set Downlink Capacity')
     plt.legend()
 
-if sys.argv[1] == '5':
-    algorithm_slots = 5
-    for n in [0,4,8,12]:
+if plotType == 5:
+    algorithm_slots = args.algorithmSlots
+    #for n in [0,4,8,12]:
+    for n in userArray:
         system_capacity = []
         for s in range(400,3300):
             nSweeps = 64/B_BS
